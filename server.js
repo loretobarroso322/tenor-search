6996b3d169c5c68850d65a6cd3601015f6f99775
Loreto Barroso <loreto.barroso@students.dominican.edu>
2020-08-31 21:37:10 -0700

    added workinggif search

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // Require Libraries
 const express = require('express');
 
+// Require tenorjs near the top of the file
+  const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "UEYJ225OH9HR", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 // App Setup
 const app = express();
 
@@ -11,15 +18,20 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 // Routes
 app.get('/', (req, res) => {
-   res.render('home')
- })
-
-app.get('/greetings/:name', (req, res) => {
-  // grab the name from the path provided
-  const name = req.params.name;
-  // render the greetings view, passing along the name
-  res.render('greetings', { name });
-})
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "10")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
 
 // example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
